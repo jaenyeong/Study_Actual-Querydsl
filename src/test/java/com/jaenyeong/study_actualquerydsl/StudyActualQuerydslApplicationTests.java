fc3fa10fa94f2ba08f79c3911518bacd98804331
Jaenyeong <jaenyeong.dev@gmail.com>
2023-03-31 15:24:41 +0900

    test(StudyActualQuerydslApplicationTests): Add default test, and add Hello entity

--- src/test/java/com/jaenyeong/study_actualquerydsl/StudyActualQuerydslApplicationTests.java
+++ src/test/java/com/jaenyeong/study_actualquerydsl/StudyActualQuerydslApplicationTests.java
@@ -1,13 +1,36 @@
 package com.jaenyeong.study_actualquerydsl;
 
+import com.jaenyeong.study_actualquerydsl.entity.Hello;
+import com.jaenyeong.study_actualquerydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class StudyActualQuerydslApplicationTests {
+
+    @Autowired
+    EntityManager em;
 
     @Test
     void contextLoads() {
-    }
+        Hello hello = new Hello();
+        em.persist(hello);
 
+        final JPAQueryFactory query = new JPAQueryFactory(em);
+        final QHello qHello = QHello.hello;
+
+        final Hello result = query.selectFrom(qHello).fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(Objects.requireNonNull(result).getId()).isEqualTo(hello.getId());
+    }
 }
