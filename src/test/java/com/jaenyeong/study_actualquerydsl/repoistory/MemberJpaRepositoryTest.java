0ba392fa8a7929524fe03f8ac7739fe061cd1a36
Jaenyeong <jaenyeong.dev@gmail.com>
2023-07-09 15:21:29 +0900

    feat(MemberJpaRepository): Add `searchByWhere` method for dynamic query, `searchConditionWithWhereTest` test method

--- src/test/java/com/jaenyeong/study_actualquerydsl/repoistory/MemberJpaRepositoryTest.java
+++ src/test/java/com/jaenyeong/study_actualquerydsl/repoistory/MemberJpaRepositoryTest.java
@@ -78,4 +78,30 @@ class MemberJpaRepositoryTest {
 
         assertThat(memberTeamDtos).extracting("username").containsExactly("member4");
     }
+
+    @Test
+    void searchConditionWithWhereTest() {
+        final Team teamA = new Team("Team A");
+        final Team teamB = new Team("Team B");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        final Member member1 = new Member("member1", 21, teamA);
+        final Member member2 = new Member("member2", 23, teamA);
+        final Member member3 = new Member("member3", 25, teamB);
+        final Member member4 = new Member("member4", 27, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        final MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(26);
+        condition.setAgeLoe(27);
+        condition.setTeamName("Team B");
+
+        final List<MemberTeamDto> memberTeamDtos = memberJpaRepository.searchByWhere(condition);
+
+        assertThat(memberTeamDtos).extracting("username").containsExactly("member4");
+    }
 }
