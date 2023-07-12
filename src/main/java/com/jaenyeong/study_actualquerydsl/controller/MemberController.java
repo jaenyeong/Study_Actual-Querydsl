660c35fe72f056d9aa1eb3a20dfd39b4acf7ae73
Jaenyeong <jaenyeong.dev@gmail.com>
2023-07-12 15:23:05 +0900

    feat(MemberController): Add `/v2/members`, `/v3/members` API

--- src/main/java/com/jaenyeong/study_actualquerydsl/controller/MemberController.java
+++ src/main/java/com/jaenyeong/study_actualquerydsl/controller/MemberController.java
@@ -3,7 +3,10 @@ package com.jaenyeong.study_actualquerydsl.controller;
 import com.jaenyeong.study_actualquerydsl.dto.MemberSearchCondition;
 import com.jaenyeong.study_actualquerydsl.dto.MemberTeamDto;
 import com.jaenyeong.study_actualquerydsl.repoistory.MemberJpaRepository;
+import com.jaenyeong.study_actualquerydsl.repoistory.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +16,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberJpaRepository memberJpaRepository;
+    private final MemberRepository memberRepository;
 
     @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
         return memberJpaRepository.searchByWhere(condition);
+    }
+
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageSimple(condition, pageable);
+    }
+
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> searchMemberV3(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageComplex(condition, pageable);
     }
 }
